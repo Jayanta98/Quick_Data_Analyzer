c68ba5200edc1024961d5f7ae28555a5bef3d23a
Jayanta <jg.16u10555@btech.nitdgp.ac.in>
2020-07-12 15:57:10 +0530

    Updated

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #This is the Quickest way to analyse any data file with(.csv) format 
 # using pandas and ProfileReport of pandas_profiling
 #It will give output in html format, if you want
+#Author: Jayanta Ghosh
 
 import pandas as pd
 from pandas_profiling import ProfileReport
@@ -9,3 +10,5 @@ df = pd.read_csv('TB_data_dictionary_2020-06-03.csv')
 print(df)
 profile = ProfileReport(df)
 profile.to_file(output_file="jay123.html")
+
+#this code help you to get the quick analysis of any (.csv) file
